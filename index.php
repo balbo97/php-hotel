ed9747fbba644703817948f1e77658f1bfd1d508
balbo97 <balbosauro1@gmail.com>
2024-01-26 01:24:13 +1000

    hotel parking filter added

--- index.php
+++ index.php
@@ -40,6 +40,25 @@
 
     ];
 
+    $filtered_hotels = $hotels;
+
+    if(isset($_GET['parking']) && $_GET['parking'] != ''){
+        $filtered_hotel = [];
+        $parking = filter_var($_GET['parking'], FILTER_VALIDATE_BOOLEAN);
+
+        foreach($hotels as $hotel){
+            if($hotel['parking'] == $parking){
+                $filtered_hotel[] = $hotel;
+            }
+        }
+
+        $filtered_hotels = $filtered_hotel;
+
+        
+    }
+
+    var_dump($filtered_hotels);
+
 ?>
 
 <!DOCTYPE html>
@@ -52,24 +71,46 @@
     <title>Document</title>
 </head>
 <body>
+    <!-- 1 - Realizzare header ed un footer in due file separati ed includerli nella pagina -->
     <?php include './partials/header.php';?>
     <main>
         <div class="container">
+            <div class="row py-5">
+                <div class="col-12">
+                    <form action="./index.php" method="GET">
+                        <div class="row">
+                            
+                            <!-- 2 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio. -->
+                            <div class="col-4">
+                                <select class="form-select" aria-label="Default select example" name="parking" id="parking">
+                                    <option selected>Select parking option</option>
+                                    <option value="true">Yes</option>
+                                    <option value="false">No</option>
+                                    
+                                </select>
+                            </div>
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-primary">Filtra</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-12">
-                <table class="table">
+                <table class="table table-striped">
                 <thead>
-                <tr>
-                    <?php foreach (array_keys($hotels[0]) as $key) { ?>
-                        <th><?php echo $key;?></th>
-                    <?php } ?>
-                </tr>
+                    <tr>
+                        <?php foreach (array_keys($hotels[0]) as $key) { ?>
+                            <th><?php echo $key;?></th>
+                        <?php } ?>
+                    </tr>
                 </thead>
                 <tbody>
                     <!-- Stampare tutti i nostri hotel con tutti i dati disponibili. -->
-                    <?php foreach ($hotels as $hotel) { ?>
+                    <?php foreach ($filtered_hotels as $hotel) { ?>
                         <tr>
-                            <td><?php echo $hotel['name']?></td>
+                            <td><?php echo $hotel['name']; ?></td>
                             <td><?php echo $hotel['description']?></td>
                             <td><?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></td>
                             <td><?php echo $hotel['vote']?></td>
@@ -83,6 +124,7 @@
             </div>
         </div>
     </main>
+    <!-- 1 - Realizzare header ed un footer in due file separati ed includerli nella pagina -->
     <?php include './partials/footer.php';?>
 </body>
 </html>
